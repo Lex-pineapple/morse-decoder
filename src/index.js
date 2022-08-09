8f66bb21510d684966257a12b7a3962de187d097
Lex <pineapplepear55@gmail.com>
2022-08-09 22:02:50 +0300

    feat: add main functional

--- src/index.js
+++ src/index.js
@@ -35,10 +35,34 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let strArr = [];
+for (let i = 0; i < expr.length; i+=10) {
+    strArr.push(expr.slice(i, i+10));
+}
+let longArr = [];
+for (let i = 0; i < strArr.length; i++) {
+    let subStr = strArr[i].substring(strArr[i].indexOf(1));
+    subStr = subStr.replaceAll('11', '-');
+    subStr = subStr.replaceAll('10', '.');
+    subStr = subStr.replaceAll('**********', ' ');
+    longArr.push(subStr);
+}
+
+for (const [key, value] of Object.entries(MORSE_TABLE)) {
+    for (let i = 0; i < longArr.length; i++) {
+        if (longArr[i] == `${key}`) {
+            longArr[i] = `${value}`;
+        }
+    }
+}
+
+longArr = longArr.join('');
+
+return longArr;
 }
 
 module.exports = {
